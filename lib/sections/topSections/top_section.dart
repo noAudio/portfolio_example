0d03769485c1d3a920dc306fbf8399b94e4f57ef
Samuel Odongo <samieodd@gmail.com>
2020-08-18 01:09:21 +0300

    Extracted menu to seperate file and fixed imports

--- lib/sections/topSections/top_section.dart
+++ lib/sections/topSections/top_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:portfolio_example/constants.dart';
 
 import 'components/logo_blur_box.dart';
+import 'components/menu.dart';
 import 'components/person_pic.dart';
 
 class TopSection extends StatelessWidget {
@@ -45,91 +46,4 @@ class TopSection extends StatelessWidget {
       ),
     );
   }
-}
-
-class Menu extends StatefulWidget {
-  @override
-  _MenuState createState() => _MenuState();
-}
-
-class _MenuState extends State<Menu> {
-  int selectedIndex = 0;
-  int hoverIndex = 0;
-  List<String> menuItems = [
-    "Home",
-    "About",
-    "Services",
-    "Portfolio",
-    "Testimonials",
-    "Contact",
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding * 2.5),
-      constraints: BoxConstraints(maxWidth: 1110.0),
-      height: 100.0,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: List.generate(
-          menuItems.length,
-          (index) => buildMenuItem(index),
-        ),
-      ),
-    );
-  }
-
-  Widget buildMenuItem(int index) => InkWell(
-        onTap: () {
-          setState(() {
-            selectedIndex = index;
-          });
-        },
-        onHover: (value) {
-          setState(() {
-            value ? hoverIndex = index : hoverIndex = selectedIndex;
-          });
-        },
-        child: Container(
-          constraints: BoxConstraints(minWidth: 122.0),
-          height: 100.0,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Text(
-                menuItems[index],
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: kTextColor,
-                ),
-              ),
-              // Hover
-              AnimatedPositioned(
-                duration: Duration(milliseconds: 200),
-                left: 0,
-                right: 0,
-                bottom:
-                    selectedIndex != index && hoverIndex == index ? -20 : -32,
-                child: Image.asset("images/Hover.png"),
-              ),
-              // Select
-              AnimatedPositioned(
-                duration: Duration(milliseconds: 200),
-                left: 0,
-                right: 0,
-                bottom: selectedIndex == index ? -2 : -32,
-                child: Image.asset("images/Hover.png"),
-              ),
-            ],
-          ),
-        ),
-      );
 }
