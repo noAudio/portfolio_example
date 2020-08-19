60db3294e0f881bf28c68a1afa879f5cc5ac007c
Samuel Odongo <samieodd@gmail.com>
2020-08-20 00:49:34 +0300

    Moved ServicCard to own file and fixed imports

--- lib/sections/service/service_section.dart
+++ lib/sections/service/service_section.dart
@@ -3,6 +3,8 @@ import 'package:portfolio_example/components/section_title.dart';
 import 'package:portfolio_example/constants.dart';
 import 'package:portfolio_example/models/Service.dart';
 
+import 'components/service_card.dart';
+
 class ServiceSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -25,79 +27,6 @@ class ServiceSection extends StatelessWidget {
                     )),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ServiceCard extends StatefulWidget {
-  const ServiceCard({
-    Key key,
-    this.index,
-  }) : super(key: key);
-
-  final int index;
-
-  @override
-  _ServiceCardState createState() => _ServiceCardState();
-}
-
-class _ServiceCardState extends State<ServiceCard> {
-  bool isHover = false;
-  Duration duration = Duration(milliseconds: 200);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      onHover: (value) {
-        setState(() {
-          isHover = value;
-        });
-      },
-      hoverColor: Colors.transparent,
-      child: AnimatedContainer(
-        duration: duration,
-        margin: EdgeInsets.symmetric(vertical: kDefaultPadding * 2),
-        height: 256.0,
-        width: 256.0,
-        decoration: BoxDecoration(
-          color: services[widget.index].color,
-          borderRadius: BorderRadius.circular(10.0),
-          boxShadow: [if (isHover) kDefaultCardShadow],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            AnimatedContainer(
-              duration: duration,
-              padding: EdgeInsets.all(kDefaultPadding * 1.5),
-              height: 120.0,
-              width: 120.0,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle,
-                boxShadow: [
-                  if (!isHover)
-                    BoxShadow(
-                      offset: Offset(0, 20.0),
-                      blurRadius: 30.0,
-                      color: Colors.black.withOpacity(0.1),
-                    ),
-                ],
-              ),
-              child: Image.asset(
-                services[widget.index].image,
-                fit: BoxFit.fill,
-              ),
-            ),
-            SizedBox(height: kDefaultPadding),
-            Text(
-              services[widget.index].title,
-              style: TextStyle(fontSize: 22.0),
-            ),
-          ],
-        ),
       ),
     );
   }
