bebb09de3da85811db4e8faef0d525293405817d
Samuel Odongo <samieodd@gmail.com>
2020-08-19 19:12:45 +0300

    Added an expanded text and duplicated it

--- lib/sections/about/about_section.dart
+++ lib/sections/about/about_section.dart
@@ -12,7 +12,40 @@ class AboutSection extends StatelessWidget {
       child: Row(
         children: [
           AboutTextWithSign(),
+          Expanded(
+            child: AboutSectionText(
+              text:
+                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            ),
+          ),
+          Expanded(
+            child: AboutSectionText(
+              text:
+                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class AboutSectionText extends StatelessWidget {
+  const AboutSectionText({
+    Key key,
+    this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Text(
+        text,
+        style: TextStyle(
+            fontWeight: FontWeight.w200, color: kTextColor, height: 2.0),
       ),
     );
   }
