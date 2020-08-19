981f04c7465d63db7d9f8e32cb08302718c52e7a
Samuel Odongo <samieodd@gmail.com>
2020-08-19 20:13:25 +0300

    Moved OutlineButton to own file and fixed imports

--- lib/sections/about/about_section.dart
+++ lib/sections/about/about_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_example/components/my_outline_button.dart';
 import 'package:portfolio_example/constants.dart';
 
 import 'components/about_section_text.dart';
@@ -11,21 +12,32 @@ class AboutSection extends StatelessWidget {
     return Container(
       margin: EdgeInsets.symmetric(vertical: kDefaultPadding * 2.0),
       constraints: BoxConstraints(maxWidth: 1110.0),
-      child: Row(
+      child: Column(
         children: [
-          AboutTextWithSign(),
-          Expanded(
-            child: AboutSectionText(
-              text:
-                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              AboutTextWithSign(),
+              Expanded(
+                child: AboutSectionText(
+                  text:
+                      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                ),
+              ),
+              ExperienceCard(numOfExp: "08"),
+              Expanded(
+                child: AboutSectionText(
+                  text:
+                      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                ),
+              ),
+            ],
           ),
-          ExperienceCard(numOfExp: "08"),
-          Expanded(
-            child: AboutSectionText(
-              text:
-                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            ),
+          SizedBox(height: kDefaultPadding * 2),
+          MyOutlineButton(
+            imageSrc: "images/hand.png",
+            text: "Hire me!",
+            press: () {},
           ),
         ],
       ),
