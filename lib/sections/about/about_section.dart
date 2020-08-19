ddb859c10d7699ff779f6d9492b3ecf251596165
Samuel Odongo <samieodd@gmail.com>
2020-08-19 19:39:20 +0300

    Moved ExperienceCard to own file, fixed imports

--- lib/sections/about/about_section.dart
+++ lib/sections/about/about_section.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:portfolio_example/constants.dart';
 
 import 'components/about_text_with_sign.dart';
+import 'components/experience_card.dart';
 
 class AboutSection extends StatelessWidget {
   @override
@@ -26,84 +27,6 @@ class AboutSection extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-class ExperienceCard extends StatelessWidget {
-  const ExperienceCard({
-    Key key,
-    this.numOfExp,
-  }) : super(key: key);
-
-  final String numOfExp;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      padding: EdgeInsets.all(kDefaultPadding),
-      height: 240.0,
-      width: 255.0,
-      decoration: BoxDecoration(
-        color: Color(0xFFF7E8FF),
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          color: Color(0xFFEDD2FC),
-          borderRadius: BorderRadius.circular(10.0),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 3.0),
-              blurRadius: 6,
-              color: Color(0xFFA600FF).withOpacity(0.25),
-            ),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                Text(
-                  numOfExp,
-                  style: TextStyle(
-                    fontSize: 100.0,
-                    fontWeight: FontWeight.bold,
-                    foreground: Paint()
-                      ..style = PaintingStyle.stroke
-                      ..strokeWidth = 6.0
-                      ..color = Color(0xFFDFA3FF).withOpacity(0.5),
-                    shadows: [
-                      BoxShadow(
-                        offset: Offset(0, 5.0),
-                        blurRadius: 10.0,
-                        color: Color(0xFFA600FF).withOpacity(0.5),
-                      ),
-                    ],
-                  ),
-                ),
-                Text(
-                  numOfExp,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 100.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: kDefaultPadding / 2),
-            Text(
-              "Years of experience",
-              style: TextStyle(
-                color: Color(0xFFA600FF),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
