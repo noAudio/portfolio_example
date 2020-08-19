b874bb1d0afe3a81d6a94bcec86372e1ac61485c
Samuel Odongo <samieodd@gmail.com>
2020-08-19 19:36:39 +0300

    Created and extracted ExperienceCard widget

--- lib/sections/about/about_section.dart
+++ lib/sections/about/about_section.dart
@@ -18,6 +18,7 @@ class AboutSection extends StatelessWidget {
                   "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore mag aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
             ),
           ),
+          ExperienceCard(numOfExp: "08"),
           Expanded(
             child: AboutSectionText(
               text:
@@ -25,6 +26,84 @@ class AboutSection extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ExperienceCard extends StatelessWidget {
+  const ExperienceCard({
+    Key key,
+    this.numOfExp,
+  }) : super(key: key);
+
+  final String numOfExp;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      padding: EdgeInsets.all(kDefaultPadding),
+      height: 240.0,
+      width: 255.0,
+      decoration: BoxDecoration(
+        color: Color(0xFFF7E8FF),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Color(0xFFEDD2FC),
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              offset: Offset(0, 3.0),
+              blurRadius: 6,
+              color: Color(0xFFA600FF).withOpacity(0.25),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Text(
+                  numOfExp,
+                  style: TextStyle(
+                    fontSize: 100.0,
+                    fontWeight: FontWeight.bold,
+                    foreground: Paint()
+                      ..style = PaintingStyle.stroke
+                      ..strokeWidth = 6.0
+                      ..color = Color(0xFFDFA3FF).withOpacity(0.5),
+                    shadows: [
+                      BoxShadow(
+                        offset: Offset(0, 5.0),
+                        blurRadius: 10.0,
+                        color: Color(0xFFA600FF).withOpacity(0.5),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  numOfExp,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 100.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: kDefaultPadding / 2),
+            Text(
+              "Years of experience",
+              style: TextStyle(
+                color: Color(0xFFA600FF),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
