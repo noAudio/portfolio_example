f44100f94abca2a669f79d6442fa8ed5ed52ae42
Samuel Odongo <samieodd@gmail.com>
2020-08-20 18:04:43 +0300

    Fixed imports

--- lib/sections/contact/contact_section.dart
+++ lib/sections/contact/contact_section.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:portfolio_example/components/section_title.dart';
 import 'package:portfolio_example/constants.dart';
 
+import 'components/social_card.dart';
+
 class ContactSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -74,64 +76,6 @@ class ContactBox extends StatelessWidget {
             ],
           )
         ],
-      ),
-    );
-  }
-}
-
-class SocialCard extends StatefulWidget {
-  const SocialCard({
-    Key key,
-    this.iconSrc,
-    this.name,
-    this.color,
-    this.press,
-  }) : super(key: key);
-
-  final String iconSrc, name;
-  final Color color;
-  final Function press;
-
-  @override
-  _SocialCardState createState() => _SocialCardState();
-}
-
-class _SocialCardState extends State<SocialCard> {
-  final Duration duration = Duration(milliseconds: 200);
-  bool isHover = false;
-  @override
-  Widget build(BuildContext context) {
-    return FittedBox(
-      child: InkWell(
-        onTap: widget.press,
-        onHover: (value) {
-          setState(() {
-            isHover = value;
-          });
-        },
-        child: AnimatedContainer(
-          duration: duration,
-          padding: EdgeInsets.symmetric(
-            vertical: kDefaultPadding / 2,
-            horizontal: kDefaultPadding * 1.5,
-          ),
-          decoration: BoxDecoration(
-            color: widget.color,
-            borderRadius: BorderRadius.circular(10.0),
-            boxShadow: [if (isHover) kDefaultShadow],
-          ),
-          child: Row(
-            children: [
-              Image.asset(
-                widget.iconSrc,
-                height: 80.0,
-                width: 80.0,
-              ),
-              SizedBox(width: kDefaultPadding),
-              Text(widget.name),
-            ],
-          ),
-        ),
       ),
     );
   }
