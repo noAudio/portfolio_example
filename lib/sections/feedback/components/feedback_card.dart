4f391122e5f7ef0ed5903a6c311fad3b2fa09125
Samuel Odongo <samieodd@gmail.com>
2020-08-20 17:24:39 +0300

    Animated hover effects

--- lib/sections/feedback/components/feedback_card.dart
+++ lib/sections/feedback/components/feedback_card.dart
@@ -16,50 +16,64 @@ class FeedbackCard extends StatefulWidget {
 }
 
 class _FeedbackCardState extends State<FeedbackCard> {
+  Duration duration = Duration(milliseconds: 200);
+  bool isHover = false;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: kDefaultPadding * 3),
-      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      height: 350.0,
-      width: 350.0,
-      decoration: BoxDecoration(
-        color: feedback[widget.index].color,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [kDefaultCardShadow],
-      ),
-      child: Column(
-        children: [
-          Transform.translate(
-            offset: Offset(0, -55.0),
-            child: Container(
-              height: 100.0,
-              width: 100.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.white, width: 10.0),
-                image: DecorationImage(
-                  image: AssetImage(feedback[widget.index].userPic),
+    return InkWell(
+      onTap: () {},
+      onHover: (value) {
+        setState(() {
+          isHover = value;
+        });
+      },
+      hoverColor: Colors.transparent,
+      child: AnimatedContainer(
+        duration: duration,
+        margin: EdgeInsets.only(top: kDefaultPadding * 3),
+        padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+        height: 350.0,
+        width: 350.0,
+        decoration: BoxDecoration(
+          color: feedback[widget.index].color,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [if (isHover) kDefaultCardShadow],
+        ),
+        child: Column(
+          children: [
+            Transform.translate(
+              offset: Offset(0, -55.0),
+              child: AnimatedContainer(
+                duration: duration,
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 10.0),
+                  boxShadow: [if (!isHover) kDefaultCardShadow],
+                  image: DecorationImage(
+                    image: AssetImage(feedback[widget.index].userPic),
+                  ),
                 ),
               ),
             ),
-          ),
-          Text(
-            feedback[widget.index].review,
-            style: TextStyle(
-              color: kTextColor,
-              fontSize: 18.0,
-              fontWeight: FontWeight.w300,
-              fontStyle: FontStyle.italic,
-              height: 1.5,
+            Text(
+              feedback[widget.index].review,
+              style: TextStyle(
+                color: kTextColor,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w300,
+                fontStyle: FontStyle.italic,
+                height: 1.5,
+              ),
             ),
-          ),
-          SizedBox(height: kDefaultPadding * 2),
-          Text(
-            "Ronald Thompson",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        ],
+            SizedBox(height: kDefaultPadding * 2),
+            Text(
+              "Ronald Thompson",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
