5d719d41bd42de0dd5fb418163a224e3e12acff9
Samuel Odongo <samieodd@gmail.com>
2020-08-20 17:42:34 +0300

    Created ContactBox widget

--- lib/sections/contact/contact_section.dart
+++ lib/sections/contact/contact_section.dart
@@ -1,8 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_example/components/section_title.dart';
+import 'package:portfolio_example/constants.dart';
 
 class ContactSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      // height: 500.0,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Color(0xFFE8F0F9),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage("images/bg_img_2.png"),
+        ),
+      ),
+      child: Column(
+        children: [
+          SizedBox(height: kDefaultPadding * 2.5),
+          SectionTitle(
+            title: "Contact Me",
+            subTitle: "For project inquiry and information",
+            color: Color(0xFF07E24A),
+          ),
+          ContactBox(),
+        ],
+      ),
+    );
+  }
+}
+
+class ContactBox extends StatelessWidget {
+  const ContactBox({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(maxWidth: 1110.0),
+      margin: EdgeInsets.only(top: kDefaultPadding * 2),
+      padding: EdgeInsets.all(kDefaultPadding * 3),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20.0),
+          topRight: Radius.circular(20.0),
+        ),
+      ),
+      child: Column(
+        children: [],
+      ),
+    );
   }
 }
