15e5fef5a5fc31485f206c2f1efb4e54ed34734f
Samuel Odongo <samieodd@gmail.com>
2020-08-20 16:07:16 +0300

    Fixed imports

--- lib/sections/recent_work/recent_work_section.dart
+++ lib/sections/recent_work/recent_work_section.dart
@@ -4,6 +4,8 @@ import 'package:portfolio_example/components/section_title.dart';
 import 'package:portfolio_example/constants.dart';
 import 'package:portfolio_example/models/RecentWork.dart';
 
+import 'components/recent_work_card.dart';
+
 class RecentWorkSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -41,58 +43,6 @@ class RecentWorkSection extends StatelessWidget {
             ),
           ),
           SizedBox(height: kDefaultPadding * 5),
-        ],
-      ),
-    );
-  }
-}
-
-class RecentWorkCard extends StatelessWidget {
-  const RecentWorkCard({
-    Key key,
-    this.index,
-  }) : super(key: key);
-
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 320.0,
-      width: 540.0,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [kDefaultCardShadow],
-      ),
-      child: Row(
-        children: [
-          Image.asset(recentWorks[index].image),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(recentWorks[index].category.toUpperCase()),
-                  SizedBox(height: kDefaultPadding / 2),
-                  Text(
-                    recentWorks[index].title,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline5
-                        .copyWith(height: 1.5),
-                  ),
-                  SizedBox(height: kDefaultPadding),
-                  Text(
-                    "View Default Padding",
-                    style: TextStyle(decoration: TextDecoration.underline),
-                  ),
-                ],
-              ),
-            ),
-          ),
         ],
       ),
     );
