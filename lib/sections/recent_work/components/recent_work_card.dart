834f3fb18b1eabd4cc48fb0659def4cb5a0bf37d
Samuel Odongo <samieodd@gmail.com>
2020-08-20 16:13:04 +0300

    Animated hover action

--- lib/sections/recent_work/components/recent_work_card.dart
+++ lib/sections/recent_work/components/recent_work_card.dart
@@ -16,45 +16,56 @@ class RecentWorkCard extends StatefulWidget {
 }
 
 class _RecentWorkCardState extends State<RecentWorkCard> {
+  bool isHover = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 320.0,
-      width: 540.0,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [kDefaultCardShadow],
-      ),
-      child: Row(
-        children: [
-          Image.asset(recentWorks[widget.index].image),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(recentWorks[widget.index].category.toUpperCase()),
-                  SizedBox(height: kDefaultPadding / 2),
-                  Text(
-                    recentWorks[widget.index].title,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline5
-                        .copyWith(height: 1.5),
-                  ),
-                  SizedBox(height: kDefaultPadding),
-                  Text(
-                    "View Default Padding",
-                    style: TextStyle(decoration: TextDecoration.underline),
-                  ),
-                ],
+    return InkWell(
+      onTap: () {},
+      onHover: (value) {
+        setState(() {
+          isHover = value;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 200),
+        height: 320.0,
+        width: 540.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [if (isHover) kDefaultCardShadow],
+        ),
+        child: Row(
+          children: [
+            Image.asset(recentWorks[widget.index].image),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(recentWorks[widget.index].category.toUpperCase()),
+                    SizedBox(height: kDefaultPadding / 2),
+                    Text(
+                      recentWorks[widget.index].title,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline5
+                          .copyWith(height: 1.5),
+                    ),
+                    SizedBox(height: kDefaultPadding),
+                    Text(
+                      "View Default Padding",
+                      style: TextStyle(decoration: TextDecoration.underline),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
